c97d57d279581312a6ae51ad5e3f6f175d35c45c
DESKTOP-4VS1R8E\ggdpc <gabrielgarciadeitos@hotmail.com>
2026-04-02 18:16:06 -0300

    FIX: Add BTCPayServer package references and fix remaining namespaces
    - Added BTCPayServer core packages as PackageReference
    - Fixed all remaining Template namespace references to Asaas
    - Updated migration files with correct namespace and DbContext
    - Plugin now has all required BTCPayServer dependencies
    - Should resolve all CS0234 and CS0246 errors

--- BTCPayServer.Plugins.Asaas/Migrations/20201117154419_Init.cs
+++ BTCPayServer.Plugins.Asaas/Migrations/20201117154419_Init.cs
@@ -2,20 +2,20 @@ using System;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace BTCPayServer.Plugins.Template.Migrations
+namespace BTCPayServer.Plugins.Asaas.Migrations
 {
-    [DbContext(typeof(MyPluginDbContext))]
+    [DbContext(typeof(AsaasDbContext))]
     [Migration("20201117154419_Init")]
     public partial class Init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.EnsureSchema(
-                name: "BTCPayServer.Plugins.Template");
+                name: "BTCPayServer.Plugins.Asaas");
 
             migrationBuilder.CreateTable(
                 name: "PluginRecords",
-                schema: "BTCPayServer.Plugins.Template",
+                schema: "BTCPayServer.Plugins.Asaas",
                 columns: table => new
                 {
                     Id = table.Column<string>(nullable: false),
@@ -31,7 +31,7 @@ namespace BTCPayServer.Plugins.Template.Migrations
         {
             migrationBuilder.DropTable(
                 name: "PluginRecords",
-                schema: "BTCPayServer.Plugins.Template");
+                schema: "BTCPayServer.Plugins.Asaas");
         }
     }
 }
